1b52e9586e342b526ce839637b79a93f7f09849f
tflion <tfleao07@gmail.com>
2021-07-22 18:10:45 -0300

    espaco-form-resolvido

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,21 +1,46 @@
 import "./Form.css"
 
-const Form = () =>{
-    return(
-        <form className="form">
-            <span>Seu nome:</span>
-            <input type="text" placeholder="Informe seu nome completo..."/>
-            <span>Seu telefone:</span>
-            <input type="text" placeholder="Seu telefone para contato..."/>
-            <span>Quem indicou:</span>
-            <input type="text" placeholder="Informe quem indicou..." />
-            <span>Tempo de atuação:</span>
-            <input type="text" placeholder="Seu tempo de atuação..." />
-            <span>Última empresa:</span>
-            <input type="text" placeholder="Sua última empresa..." />
-            <span>Atividades realizadas:</span>
-            <textarea placeholder="Atividades realizas..."></textarea>
-        </form>
+const Form = () => {
+    return (
+        <section className="formulario">
+            <div className="form-content">
+                <form>
+                    <div className="form-wrapper w50">
+                        <span>Seu nome nompleto:</span>
+                        <input type="text" />
+                    </div>
+                    <div className="form-wrapper w50">
+                        <span>Seu telefone:</span>
+                        <input type="text" />
+                    </div>
+                    <div className="form-wrapper w100">
+                        <span>Quem indicou:</span>
+                        <input type="text" />
+                    </div>
+                    <div className="form-wrapper w50">
+                        <span>Tempo de atuação:</span>
+                        <input type="text" />
+                    </div>
+                    <div className="form-wrapper w50">
+                        <span>Área de atuação:</span>
+                        <input type="text" />
+                    </div>
+                    <div className="form-wrapper w100">
+                        <span>Última empresa: </span>
+                        <input type="text" />
+                    </div>
+                    <div className="form-wrapper w100">
+                        <span>Atividades Realizadas: </span>
+                        <textarea></textarea>
+                    </div>
+                    <div className="form-wrapper w100">
+                        <input type="submit" value="Preencher Formulário" />
+                    </div>
+
+                </form>
+                <div className="clear"></div>
+            </div>
+        </section>
     );
 }
 
