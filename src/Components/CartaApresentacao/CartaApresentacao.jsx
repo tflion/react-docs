6dc7017e2b2210d792d451a4f883cbec4738233f
tflion <tfleao07@gmail.com>
2021-07-28 20:56:12 -0300

    estados-add-no-form

--- src/Components/CartaApresentacao/CartaApresentacao.jsx
+++ src/Components/CartaApresentacao/CartaApresentacao.jsx
@@ -8,9 +8,9 @@ const CartaApresentacao = (props) => {
             <h2 className="titulo-doc">Carta de Apresentação</h2>
             <div className="folha">
                 <p>Prezado(a) Senhor(a),</p>
-                <p>Conforme a indicação de ( NOME DE QUEM INDICOU ), envio meu currículo para apreciação.</p>
-                <p>Há ( TEMPO ) atuo na área de (ÁREA ATUANTE) da empresa ( NOME DA EMPRESA ),
-                    realizando atividades de ( ATIVIDADES REALIZADAS ).</p>
+                <p>Conforme a indicação de {props.dados.quemIndica}, envio meu currículo para apreciação.</p>
+                <p>Há {props.dados.tempo} atuo na área de {props.dados.areaAtuacao} da empresa {props.dados.empresa},
+                    realizando atividades de {props.dados.atividadesRealizadas}.</p>
                 <p>Neste momento, busco novos desafios no mercado,
                     visando o desenvolvimento de um trabalho objetivo e gerador de resultados,
                     de forma a possibilitar o crescimento qualitativo e quantitativo para os envolvidos.</p>
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -5,16 +5,33 @@ import "./Form.css"
 const Form = (props) => {
     const [nomeUsuario, setNomeUsuario] = useState('');
     const [telefoneUsuario, setTelefoneUsuario] = useState('');
+    const [quemIndicou, setQuemIndicou] = useState('');
+    const [tempo, setTempo] = useState('');
+    const [areaAtuacao, setAreaAtuacao] = useState('');
+    const [empresa, setEmpresa] = useState('');
+    const [atividadesRealizadas, setAtividadesRealizadas] = useState('');
 
     const trocarNomeUsuarioHandler = (event) => {
         setNomeUsuario(event.target.value);
     };
-
     const trocarTelefoneUsuarioHandler = (event) => {
         setTelefoneUsuario(event.target.value);
     };
-
-    
+    const trocarQuemIndicouHandler = (event) => {
+        setQuemIndicou(event.target.value);
+    };
+    const trocarTempoHandler = (event) => {
+        setTempo(event.target.value);
+    };
+    const trocarAreaAtuacaoHanddler = (event) => {
+        setAreaAtuacao(event.target.value);
+    };
+    const trocarEmpresaHandler = (event) => {
+        setEmpresa(event.target.value);
+    };
+    const trocarAtividadesRealizadasHandler = (event) => {
+        setAtividadesRealizadas(event.target.value);
+    };
 
 
     const enviarHandler = (event) => {
@@ -43,28 +60,38 @@ const Form = (props) => {
                     <div className="form-wrapper w50">
                         <span>Seu telefone:</span>
                         <input type="text"
-                        value={telefoneUsuario}
-                        onChange={trocarTelefoneUsuarioHandler} />
+                            value={telefoneUsuario}
+                            onChange={trocarTelefoneUsuarioHandler} />
                     </div>
                     <div className="form-wrapper w100">
                         <span>Quem indicou:</span>
-                        <input type="text" />
+                        <input type="text"
+                            onChange={trocarQuemIndicouHandler}
+                            value={quemIndicou} />
                     </div>
                     <div className="form-wrapper w50">
                         <span>Tempo de atuação:</span>
-                        <input type="text" />
+                        <input type="text"
+                            onChange={trocarTempoHandler}
+                            value={tempo} />
                     </div>
                     <div className="form-wrapper w50">
                         <span>Área de atuação:</span>
-                        <input type="text" />
+                        <input type="text"
+                            onChange={trocarAreaAtuacaoHanddler}
+                            value={areaAtuacao} />
                     </div>
                     <div className="form-wrapper w100">
                         <span>Última empresa: </span>
-                        <input type="text" />
+                        <input type="text"
+                            onChange={trocarEmpresaHandler}
+                            value={empresa} />
                     </div>
                     <div className="form-wrapper w100">
                         <span>Atividades Realizadas: </span>
-                        <textarea></textarea>
+                        <textarea
+                            onChange={trocarAtividadesRealizadasHandler}
+                            value={atividadesRealizadas}></textarea>
                     </div>
                     <div className="form-wrapper w100">
                         <input type="submit"
