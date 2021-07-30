5a9c4b417c19b9598f1b3c43bcde4fe62462675f
tflion <tfleao07@gmail.com>
2021-07-30 11:39:02 -0300

    usando-refs-e-modal-completo

--- src/Components/UI/Form/Form.jsx
+++ src/Components/UI/Form/Form.jsx
@@ -1,45 +1,38 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import ErroModal from '../ErroModal/ErroModal';
 import "./Form.css"
 
 const Form = (props) => {
-    const [nomeUsuario, setNomeUsuario] = useState('');
-    const [telefoneUsuario, setTelefoneUsuario] = useState('');
-    const [quemIndicou, setQuemIndicou] = useState('');
-    const [tempo, setTempo] = useState('');
-    const [areaAtuacao, setAreaAtuacao] = useState('');
-    const [empresa, setEmpresa] = useState('');
-    const [atividadesRealizadas, setAtividadesRealizadas] = useState('');
-
-    const trocarNomeUsuarioHandler = (event) => {
-        setNomeUsuario(event.target.value);
-    };
-    const trocarTelefoneUsuarioHandler = (event) => {
-        setTelefoneUsuario(event.target.value);
-    };
-    const trocarQuemIndicouHandler = (event) => {
-        setQuemIndicou(event.target.value);
-    };
-    const trocarTempoHandler = (event) => {
-        setTempo(event.target.value);
-    };
-    const trocarAreaAtuacaoHanddler = (event) => {
-        setAreaAtuacao(event.target.value);
-    };
-    const trocarEmpresaHandler = (event) => {
-        setEmpresa(event.target.value);
-    };
-    const trocarAtividadesRealizadasHandler = (event) => {
-        setAtividadesRealizadas(event.target.value);
-    };
+    const nomeInputRef = useRef();
+    const telefoneInputRef = useRef();
+    const indicacaoInputRef = useRef();
+    const tempoInputRef = useRef();
+    const areaAtuacaoInputRef = useRef();
+    const empresaInputRef = useRef();
+    const atividadesRealizadasInputRef = useRef();
 
     const [erro, setErro] = useState();
 
     const enviarHandler = (event) => {
         event.preventDefault();
+        //Pegando os valores dos inputs via Refs
+        const nomeInserido = nomeInputRef.current.value;
+        const telefoneInserido = telefoneInputRef.current.value;
+        const indicacaoInserido = indicacaoInputRef.current.value;
+        const tempoInserido = tempoInputRef.current.value;
+        const areaInserido = areaAtuacaoInputRef.current.value;
+        const empresaInserido = empresaInputRef.current.value;
+        const atividadesInserido = atividadesRealizadasInputRef.current.value;
 
-        if(nomeUsuario.trim().length === 0 ){
+        //Verificando se há campos vazios nos inputs
+        if (nomeInserido.trim().length === 0 ||
+            telefoneInserido.trim().length === 0 ||
+            indicacaoInserido.trim().length === 0 ||
+            tempoInserido.trim().length === 0 ||
+            areaInserido.trim().length === 0 ||
+            empresaInserido.trim().length === 0 ||
+            atividadesInserido.trim().length === 0) {
             setErro({
                 titulo: 'Dados Incompletos',
                 mensagem: 'Favor preencher os campos em branco (Sem campos vazios).',
@@ -48,20 +41,38 @@ const Form = (props) => {
             return;
         }
 
-        props.onEnviarDadosDocumento(nomeUsuario, telefoneUsuario, quemIndicou, tempo, areaAtuacao, empresa, atividadesRealizadas);
-        setTelefoneUsuario('');
-        setNomeUsuario('');
-        setQuemIndicou('');
-        setTempo('');
-        setEmpresa('');
-        setAtividadesRealizadas('');
-        setAreaAtuacao('');
+        //Enviando dados via props para DocsApp
+        props.onEnviarDadosDocumento(
+            nomeInserido,
+            telefoneInserido,
+            indicacaoInserido,
+            tempoInserido,
+            areaInserido,
+            empresaInserido,
+            atividadesInserido);
+
+        limparInputAposEnvio();
+
     }
 
+    const limparInputAposEnvio = () => {
+        nomeInputRef.current.value = '';
+        telefoneInputRef.current.value = '';
+        atividadesRealizadasInputRef.current.value = '';
+        empresaInputRef.current.value = '';
+        areaAtuacaoInputRef.current.value = '';
+        indicacaoInputRef.current.value = '';
+        tempoInputRef.current.value = '';
+    }
+
+    const erroHandler = () => {
+        setErro(null);
+    }
 
     return (
+        //Se erro for true o modal Aparece com as strings informadas acima
         <>
-            {erro && <ErroModal titulo={erro.titulo} conteudo={erro.mensagem} />}
+            {erro && <ErroModal titulo={erro.titulo} conteudo={erro.mensagem} onConfirm={erroHandler} />}
             <section className="formulario">
                 <div className="form-content">
                     <form onSubmit={enviarHandler}>
@@ -71,51 +82,43 @@ const Form = (props) => {
                         <div className="form-wrapper w50">
                             <span>Seu nome nompleto:</span>
                             <input type="text"
-                                value={nomeUsuario}
-                                onChange={trocarNomeUsuarioHandler} />
+                                ref={nomeInputRef} />
                         </div>
                         <div className="form-wrapper w50">
                             <span>Seu telefone:</span>
                             <input type="text"
-                                value={telefoneUsuario}
-                                onChange={trocarTelefoneUsuarioHandler} />
+                                ref={telefoneInputRef} />
                         </div>
                         <div className="form-wrapper w100">
                             <span>Quem indicou:</span>
                             <input type="text"
-                                onChange={trocarQuemIndicouHandler}
-                                value={quemIndicou} />
+                                ref={indicacaoInputRef} />
                         </div>
                         <div className="form-wrapper w50">
                             <span>Tempo de atuação:</span>
                             <input type="text"
-                                onChange={trocarTempoHandler}
-                                value={tempo} />
+                                ref={tempoInputRef} />
                         </div>
                         <div className="form-wrapper w50">
                             <span>Área de atuação:</span>
                             <input type="text"
-                                onChange={trocarAreaAtuacaoHanddler}
-                                value={areaAtuacao} />
+                                ref={areaAtuacaoInputRef} />
                         </div>
                         <div className="form-wrapper w100">
                             <span>Última empresa: </span>
                             <input type="text"
-                                onChange={trocarEmpresaHandler}
-                                value={empresa} />
+                                ref={empresaInputRef} />
                         </div>
                         <div className="form-wrapper w100">
                             <span>Atividades Realizadas: </span>
                             <textarea
-                                onChange={trocarAtividadesRealizadasHandler}
-                                value={atividadesRealizadas}></textarea>
+                                ref={atividadesRealizadasInputRef}></textarea>
                         </div>
                         <div className="form-wrapper w100">
                             <input type="submit"
                                 value="Preencher Formulário"
                             />
                         </div>
-
                     </form>
                     <div className="clear"></div>
                 </div>
